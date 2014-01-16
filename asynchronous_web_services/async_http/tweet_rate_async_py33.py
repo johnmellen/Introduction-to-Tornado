0de004f28b5514e3acda4b7e2b6840ebe8f419a6
John Mellen <johnhenrymellen@gmail.com>
2014-01-16 13:39:06 +0000

    Python 3.3 fixes for the tweet_rate examples
    Fixes to get tweet_rate.py, tweet_rate_oauth.py, and tweet_rate_gen.py
    to run with Python 3.3.  Notably, the urllib import statement has
    changed, together with the urlencode function call (urlencode now lives
    in urllib.parse).  Also, the contents of response.body (a 'bytes'
    object) needs to be decoded before it's passed to json.loads().

---
+++ asynchronous_web_services/async_http/tweet_rate_async_py33.py
@@ -0,0 +1,52 @@
+# tweet_rate_async_py33.py   Tested with Python 3.3
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+
+from urllib import parse
+import json
+import datetime
+import time
+
+from tweet_rate_oauth_py33 import oauth
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	def get(self):
+		query = self.get_argument('q')
+		client = tornado.httpclient.AsyncHTTPClient()
+		url = "https://api.twitter.com/1.1/search/tweets.json?" + \
+                      parse.urlencode({"q": query, "result_type": "recent", "count": 100})
+		client.fetch( url, callback=self.on_response, headers=oauth(url))
+
+	def on_response(self, response):
+		response_body = response.body.decode()
+		body = json.loads(response_body)
+		result_count = len(body['statuses'])
+		now = datetime.datetime.utcnow()
+		raw_oldest_tweet_at = body['statuses'][-1]['created_at']
+		oldest_tweet_at = datetime.datetime.strptime(raw_oldest_tweet_at,
+				"%a %b %d %H:%M:%S +0000 %Y")
+		seconds_diff = time.mktime(now.timetuple()) - \
+				time.mktime(oldest_tweet_at.timetuple())
+		tweets_per_second = float(result_count) / seconds_diff
+		self.write("""
+<div style="text-align: center">
+	<div style="font-size: 72px">%s</div>
+	<div style="font-size: 144px">%.02f</div>
+	<div style="font-size: 24px">tweets per second</div>
+</div>""" % (self.get_argument('q'), tweets_per_second))
+		self.finish()
+
+if __name__ == "__main__":
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
